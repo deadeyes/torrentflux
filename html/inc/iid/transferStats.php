583e73eff9588d86f9569b09488e88b4a0cdcf96
deadeyes <deadeyes@torrentflux-ng>
2010-12-26 00:31:04 +0100

    Refactored the code for transferStats page

--- html/inc/iid/transferStats.php
+++ html/inc/iid/transferStats.php
@@ -38,113 +38,23 @@ require_once('inc/functions/functions.transfer.php');
 // init template-instance
 tmplInitializeInstance($cfg["theme"], "page.transferStats.tmpl");
 
-// TODO: Rename function name
-function transmissionSetVars ($transfer, $tmpl) {
+// IMPORTANT: needed here otherwhise $transfer is not set
+transfer_init();
 
+if ($cfg["transmission_rpc_enable"]) {
 	require_once('inc/functions/functions.rpc.transmission.php');
-	$options = array("eta","percentDone", "rateDownload", "rateUpload", "downloadedEver", "uploadedEver", "percentDone", "sizeWhenDone");
-	$returnArr = getTransmissionTransfer($transfer, $options);
-
-	$tmpl->setvar('transferowner', getTransmissionTransferOwner($transfer));
-
-	$tmpl->setvar('size', @formatBytesTokBMBGBTB($returnArr['sizeWhenDone']));
-
-	// sharing
-	$tmpl->setvar('sharing', ($returnArr["downloadedEver"] > 0) ? @number_format((($returnArr["uploadedEver"] / $returnArr["downloadedEver"]) * 100), 2) : "0");
-
-	// totals
-	$tmpl->setvar('downTotal', @formatFreeSpace($returnArr["downloadedEver"] / 1048576));
-	$tmpl->setvar('upTotal', @formatFreeSpace($returnArr["uploadedEver"] / 1048576));
-
-	// port + cons
-	//$tmpl->setvar('size', @formatBytesTokBMBGBTB($transferSize));
-
-	$isRunning = true;
-	if ($isRunning) {
-		$tmpl->setvar('running', 1);
-
-		// current totals
-		$tmpl->setvar('downTotalCurrent', formatFreeSpace($totalsCurrent["downtotal"] / 1048576));
-		$tmpl->setvar('upTotalCurrent', formatFreeSpace($totalsCurrent["uptotal"] / 1048576));
-
-		// seeds + peers
-		$tmpl->setvar('seeds', $sf->seeds);
-		$tmpl->setvar('peers', $sf->peers);
-
-		// port + cons
-		$transfer_pid = getTransferPid($transfer);
-		$tmpl->setvar('port', netstatPortByPid($transfer_pid));
-		$tmpl->setvar('cons', netstatConnectionsByPid($transfer_pid));
-
-		// up speed
-		$tmpl->setvar('up_speed', (trim($returnArr['rateUpload']) != "") ? formatBytesTokBMBGBTB( $returnArr['rateUpload'] ) . '/s' : '0.0 kB/s');
-
-		// down speed
-		$tmpl->setvar('down_speed', (trim($returnArr['rateDownload']) != "") ? formatBytesTokBMBGBTB( $returnArr['rateDownload'] ) . '/s' : '0.0 kB/s');
-
-		// sharekill
-		$tmpl->setvar('sharekill', ($ch->sharekill != 0) ? $ch->sharekill.'%' : '&#8734');
-	} else {
-
-		// running
-		$tmpl->setvar('running', 0);
-
-		// current totals
-		$tmpl->setvar('downTotalCurrent', "");
-		$tmpl->setvar('upTotalCurrent', "");
-
-		// seeds + peers
-		$tmpl->setvar('seeds', "");
-		$tmpl->setvar('peers', "");
-
-		// port + cons
-		$tmpl->setvar('port', "");
-		$tmpl->setvar('cons', "");
-
-		// up speed
-		$tmpl->setvar('up_speed', "");
-
-		// down speed
-		$tmpl->setvar('down_speed', "");
-
-		// sharekill
-		$tmpl->setvar('sharekill', "");
-
-	}
-
-	if ($returnArr['eta'] < 0)
-		$tmpl->setvar('time_left', 'n/a');
-	else
-		$tmpl->setvar('time_left', convertTime( $returnArr['eta'] ));
-
-	// graph width
-	$tmpl->setvar('graph_width1', $returnArr['percentDone']*100);
-	$tmpl->setvar('graph_width2', (100 - $returnArr['percentDone']*100));
-		
-	$tmpl->setvar('percent_done', $returnArr['percentDone']*100);
-
-	// language vars
-	global $cfg;
-	$tmpl->setvar('_USER', $cfg['_USER']);
-	$tmpl->setvar('_SHARING', $cfg['_SHARING']);
-	$tmpl->setvar('_ID_CONNECTIONS', $cfg['_ID_CONNECTIONS']);
-	$tmpl->setvar('_ID_PORT', $cfg['_ID_PORT']);
-	$tmpl->setvar('_DOWNLOADSPEED', $cfg['_DOWNLOADSPEED']);
-	$tmpl->setvar('_UPLOADSPEED', $cfg['_UPLOADSPEED']);
-	$tmpl->setvar('_PERCENTDONE', $cfg['_PERCENTDONE']);
-	$tmpl->setvar('_ESTIMATEDTIME', $cfg['_ESTIMATEDTIME']);
-
-	return $tmpl;
+	$isTransTransfer = isTransmissionTransfer($transfer);
+} else {
+	$isTransTransfer = false;
 }
 
-$isTransmissionTransfer = true;
-if ($isTransmissionTransfer) {
-	transfer_init();
+if ($isTransTransfer) {
+	require_once('inc/functions/functions.ui.transmission.php');
 	$tmpl = transmissionSetVars($transfer, $tmpl);
 } else { 
 
 	// init transfer
-	transfer_init();
+	//transfer_init(); // Done at the start
 
 	// stat
 	$transferowner = getOwner($transfer);
